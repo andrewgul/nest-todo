47e436b7de2ec9c9ebdb7f4358de3c6fc2b7ec2b
Andrew Gulin <a.gulin@ktsstudio.ru>
2024-05-07 16:29:27 +0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const GLOBAL_PREFIX = 'api';
 
@@ -7,6 +8,18 @@ const PORT = 5555;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Nest TODO')
+    .setDescription('My first NestJS project!')
+    .setVersion('0.0.1')
+    .addTag('todos')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentConfig);
+
+  SwaggerModule.setup('docs', app, document);
+
   app.setGlobalPrefix(GLOBAL_PREFIX);
 
   await app.listen(PORT);
