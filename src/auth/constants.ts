db3006124269016a6cf0441d2fe034ff3ad637eb
Andrew Gulin <a.gulin@ktsstudio.ru>
2024-05-07 03:04:56 +0300

    feat: add auth mechanism

---
+++ src/auth/constants.ts
@@ -0,0 +1,7 @@
+export const JWT_CONSTANTS = {
+  /**
+   * Его понятно дело нужно хранить не тут а в переменных окружения
+   */
+  superSecretKey:
+    'Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you',
+};
